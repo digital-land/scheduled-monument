ca2ee1caa1c258d5f493c514d41e23a0ef44278f
Paul Downey <psd@whatfettle.com>
2021-11-08 18:51:25 +0000

    Redirected

--- Makefile
+++
@@ -1,5 +0,0 @@
-COLLECTION=historic-england
-DATASET=scheduled-monument
-
-include makerules/makerules.mk
-include makerules/render.mk
---
+++ redirect.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+import csv
+from pathlib import Path
+
+site="https://www.digital-land.info/"
+dataset = Path().cwd().name
+print("dataset:", dataset)
+
+
+def redirect(path, to):
+    Path(path).parent.mkdir(parents=True, exist_ok=True)
+    with open(path, 'w') as f:
+        f.write('<meta http-equiv="refresh" content="0; url=%s%s">' % (site, to))
+
+
+redirect("./docs/index.html", "dataset/%s" % dataset)
+
+for row in csv.DictReader(open("../entity-builder/dataset/entity.csv")):
+    if row["dataset"] != dataset:
+        continue
+
+    path = Path("./docs/%s/index.html" % (row["reference"]))
+    redirect(path, "entity/" + row["entity"])
